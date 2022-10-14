80c763dc51cc817e3d431bb8bca6ec2f594ee6a1
hoaingodong1 <hoai.ngo23@student.passerellesnumeriques.org>
2022-10-14 17:01:51 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,219 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function FirstScreen() {
+  const[selectedValue, setSelectedValue] = useState('ltr');
+  const values = ['ltr', 'rtl'];
+  return(
+    <View style={{padding: 10, flex: 1}}>
+      <View style={styles.row}>
+        {values.map(value => (
+          <TouchableOpacity
+            key={value}
+            onPress={() => setSelectedValue (value)}
+            style={[styles.button, selectedValue === value && styles.selected]}>
+            <Text
+              style={[
+                styles.buttonLabel,
+                selectedValue === value && styles.selectedLabel,
+              ]}>
+              {value}
+            </Text>
+          </TouchableOpacity>
+         ))}
+         </View>
+         <View>
+          <View style ={[selectedValue.container, {direction: selectedValue}]}>
+            <View style={{width: 50, height: 50, backgroundColor:'#EE2C38'}} />
+            <View style={{width: 50, height: 50, backgroundColor:'#FAA030'}} />
+            <View style={{width: 50, height: 50, backgroundColor:'#32B76C'}} />
+          </View>
+         </View>
+      </View>
+  );
+
+}
+
+function SecondScreen() {
+  const [position, setPosition] = useState('relative');
+  const values = ['relative', 'absolute'];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 10, flex: 1}}>
+      <View style={styles.row}>
+        {values.map(value => (
+          <TouchableOpacity
+            key={value}
+            onPress={() => setPosition(value)}
+            style={[styles.button, position === value && styles.selected]}>
+            <Text
+              style={[
+                styles.buttonLabel,
+                position === value && styles.selectedLabel,
+              ]}>
+              {value}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.box,
+            {
+              top: 25,
+              left: 25,
+              position,
+              backgroundColor: 'powderblue',
+            },
+          ]}
+        />
+        <View
+          style={[
+            styles.box,
+            {
+              top: 50,
+              left: 50,
+              position,
+              backgroundColor: 'skyblue',
+            },
+          ]}
+        />
+        <View
+          style={[
+            styles.box,
+            {
+              top: 75,
+              left: 75,
+              position,
+              backgroundColor: 'steelblue',
+            },
+          ]}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+
+function ThirdScreen(){
+  const [justifyContent, setJustifyContent] = useState("flex-start");
+  return (
+    <PreviewLayout
+      label="justifyContent"
+      selectedValue={justifyContent}
+      values={[
+        "flex-start",
+        "flex-end",
+        "center",
+        "space-between",
+        "space-around",
+        "space-evenly",
+      ]}
+      setSelectedValue={setJustifyContent}
+    >
+      <View style={{width: 50, height: 50, backgroundColor:'#EE2C38'}} />
+      <View style={{width: 50, height: 50, backgroundColor:'#FAA030'}} />
+      <View style={{width: 50, height: 50, backgroundColor:'#32B76C'}} />
+    </PreviewLayout>
+  );
+}
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, { [label]: selectedValue }]}>
+      {children}
+    </View>
+  </View>
+);
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="direction" component={FirstScreen} />
+      <Tab.Screen name="position" component={SecondScreen} />
+      <Tab.Screen name="justify" component={ThirdScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 8,
+    backgroundColor: 'grey',
   },
-});
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: '48%',
+    textAlign: 'center',
+  },
+  selected: {
+    backgroundColor: 'coral',
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'coral',
+  },
+  selectedLabel: {
+    color: 'white',
+  },
+  label: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 24,
+  },
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
